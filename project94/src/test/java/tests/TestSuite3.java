f4f1319b1d4b8d0df98775508bf26b89ca0fab38
kishore <skill2automate@gmail.com>
2025-05-18 10:06:36 +0530

    samepl project updated for jenkins run

---
+++ project94/src/test/java/tests/TestSuite3.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuite3 {
+
+	@Test
+	public void test1() {
+	System.out.println("test1");	
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");	
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");	
+	}
+
+}
